cd99e0dd0deb743cad77e2c90d3db4b218088523
Vedat <svmlvdt@gmail.com>
2021-09-23 12:06:37 +0300

    son duzenlemeler yapildi

--- Tic Tac/script.js
+++ Tic Tac/script.js
@@ -13,8 +13,7 @@ replayBtn = resultBox.querySelector("button");
 window.onload = () =>{
 
     for (let i = 0; i < allBox.length; i++) {
-        allBox[i].setAttribute("onclick" , "clickedBox(this)");
-        
+        allBox[i].setAttribute("onclick" , "clickedBox(this)");    
     }
 
     selectX_Btn.onclick = () => {
@@ -52,7 +51,9 @@ function  clickedBox(element) {
    selectWinner();
    players.style.pointerEvents = "none";
     element.style.pointerEvents = "none";//ayni elemente ayni islem bir daha yapilmasin icin yani eventi bir kere uygula demek
-    let randomDelayTime = ((Math.random() * 1000)+400).toFixed();
+    
+    //bilgisayarin biraz gecikmeli oynamasi icin
+    let randomDelayTime = ((Math.random() * 1000)+200).toFixed();
     console.log(randomDelayTime);
     setTimeout( () => {
         bot(runBot);
@@ -68,9 +69,8 @@ function  bot() {
     for (let i = 0; i < allBox.length; i++) {
         if (allBox[i].childElementCount == 0) {//eger spanin cocugu yoksa yani i elementi yani tiklanmamissa bossa
             array.push(i);
-           // console.log(i + ".nci index numarali "+" spanin icinde element yok")
-        }
-        
+            console.log(i+1 + ".nci index numarali "+" spanin icinde element yok")
+        }       
     }
     let rndBox = array[Math.floor(Math.random()*array.length)];
     console.log(rndBox);
@@ -110,7 +110,7 @@ function cehckClass(val1, val2, val3, sign) {
 function  selectWinner() {
     if (cehckClass(1,2,3,playerSign) || cehckClass(4,5,6,playerSign) || cehckClass(6,7,8,playerSign) || cehckClass(1,4,7,playerSign) || cehckClass(2,5,8,playerSign) || cehckClass(3,6,9,playerSign) ||cehckClass(1,5,9,playerSign) || cehckClass(3,5,7,playerSign) ) 
     {
-        console.log(playerSign +" "+ " kazandi");
+        console.log(playerSign +" "+ " hat gewonnen");
         runBot = false; //passing the false boolen value to runBot so bot won't run again
         bot(runBot); //calling bot function
         setTimeout(()=>{ //after match won by someone then hide the playboard and show the result box after 700ms
@@ -125,8 +125,8 @@ function  selectWinner() {
             setTimeout(()=>{ //after match drawn then hide the playboard and show the result box after 700ms
                 resultBox.classList.add("show");
                 playBoard.classList.remove("show");
-            }, 700); //1s = 1000ms
-            wonText.textContent = "Ausgeleic!"; //displaying draw match text
+            }, 500); 
+            wonText.textContent = "Ausgeleich!"; //displaying draw match text
         }
     }
 }
